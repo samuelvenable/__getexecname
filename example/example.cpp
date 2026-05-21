b56869a3a08502c65a85526039d7b1da13d936e0
openbsd <openbsd@OpenBSD.lan>
2026-05-21 00:24:07 -0400

    yay

--- example/example.cpp
+++ example/example.cpp
@@ -2,9 +2,15 @@
 #include <cctype>
 #include <cstring>
 #include <cstdlib>
+#if defined(__external__)
 #include <__getexecname/external.h>
 #include <__getbasepath/external.h>
 #include <__getprogname/external.h>
+#else
+#include <__getexecname/internal.h>
+#include <__getbasepath/internal.h>
+#include <__getprogname/internal.h>
+#endif
 int main(int argc, char **argv) {
   if (argc <= 1) {
     const char *execname = __getexecname();
@@ -13,10 +19,14 @@ int main(int argc, char **argv) {
     printf("__getexecname(): %s\n__getbasepath(): %s\n__getprogname(): %s\n", execname, basepath, progname);
     return ((!execname || !basepath || !progname) ? 0 : 1);
   }
+  #if defined(__external__)
   long long pid = strtoll(argv[1], nullptr, 10);
   const char *execname = __getexecname(pid);
   const char *basepath = __getbasepath(pid);
   const char *progname = __getprogname(pid);
   printf("__getexecname(%lld): %s\n__getbasepath(%lld): %s\n__getprogname(%lld): %s\n", pid, execname, pid, basepath, pid, progname);
   return ((!execname || !basepath || !progname) ? 0 : 2);
+  #else
+  return 2;
+  #endif
 }
--- example/example.sh
+++ example/example.sh
@@ -1,27 +1,54 @@
 #!/bin/sh
 cd "${0%/*}/../"
+# build internal example
 if [ $(uname) = "Darwin" ]; then
-  clang++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall -arch arm64 -arch x86_64;c
+  clang++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall -arch arm64 -arch x86_64;c
 elif [ $(uname) = "Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out -1;
+    g++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./internal;
   else
-    clang++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall; ./a.out -1;
+    clang++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall; ./internal;
   fi
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall -static; ./a.out -1;
+  clang++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall -static; ./internal;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out -1;
+  g++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./internal;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out -1;
+  g++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./internal;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall -lkvm -static; ./a.out -1;
+  clang++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall -lkvm -static; ./internal;
 elif [ $(uname) = "SunOS" ]; then
-  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall -lproc -static-libgcc; ./a.out -1;
+  g++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall -lproc -static-libgcc; ./internal;
 elif [ $(uname) = "Haiku" ]; then
-  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out -1;
+  g++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./internal;
 elif [ $(uname) = "QNX" ]; then
-  q++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out -1;
+  q++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./internal;
 else
-  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o a.out.exe -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./a.out.exe -1;
+  g++ __getexecname/internal.cpp __getbasepath/internal.cpp __getprogname/internal.cpp example/example.cpp -o internal.exe -I. -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./internal.exe -1;
+fi
+# build external example
+if [ $(uname) = "Darwin" ]; then
+  clang++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall -arch arm64 -arch x86_64;c
+elif [ $(uname) = "Linux" ]; then
+  if [ -f "/bin/g++" ]; then
+    g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall -static-libgcc -static-libstdc++ -static; ./external -1;
+  else
+    clang++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall; ./external -1;
+  fi
+elif [ $(uname) = "FreeBSD" ]; then
+  clang++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall -static; ./external -1;
+elif [ $(uname) = "DragonFly" ]; then
+  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall -static-libgcc -static-libstdc++ -static; ./external -1;
+elif [ $(uname) = "NetBSD" ]; then
+  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall -static-libgcc -static-libstdc++ -static; ./external -1;
+elif [ $(uname) = "OpenBSD" ]; then
+  clang++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall -lkvm -static; ./external -1;
+elif [ $(uname) = "SunOS" ]; then
+  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall -lproc -static-libgcc; ./external -1;
+elif [ $(uname) = "Haiku" ]; then
+  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall -static-libgcc -static-libstdc++ -static; ./external -1;
+elif [ $(uname) = "QNX" ]; then
+  q++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external -I. -std=c++17 -D__external__ -Wall -static-libgcc -static-libstdc++ -static; ./external -1;
+else
+  g++ __getexecname/external.cpp __getbasepath/external.cpp __getprogname/external.cpp example/example.cpp -o external.exe -I. -std=c++17 -D__external__ -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./external.exe -1;
 fi

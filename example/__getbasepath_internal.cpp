0f7330ebe247f4653993f9a50f23a1e1ef4ee46f
high on tantor <josephvenable@verizon.net>
2026-05-20 21:44:10 -0400

    Delete example directory

--- example/__getbasepath_internal.cpp
+++
@@ -1,9 +0,0 @@
-// Code for current executable's base path:
-#include <stdio.h>
-#include <__getbasepath/internal.h>
-int main() {
-  const char *internal = __getbasepath();
-  printf("Possible Output: %s\n", internal);
-  // error when __getbasepath() failed.
-  return internal ? 0 : 1;
-}
--- example/__getexecname_internal.cpp
+++
@@ -1,9 +0,0 @@
-// Code for current executable's exec name:
-#include <stdio.h>
-#include <__getexecname/internal.h>
-int main() {
-  const char *internal = __getexecname();
-  printf("Possible Output: %s\n", internal);
-  // error when __getexecname() failed.
-  return internal ? 0 : 1;
-}
